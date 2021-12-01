8494c753a5de75a43ec71caeaa5eff660dc31b4c
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-12-01 16:46:44 -0600

    optional interface props

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,12 @@ const user = new User({ name: 'myname', age: 20 });
 
 console.log(user.get('name'));
 console.log(user.get('age'));
+
+user.set({ name: 'newname', age: 999 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
+
+user.set({ name: 'anothername'});
+
+console.log(user.get('name'));
