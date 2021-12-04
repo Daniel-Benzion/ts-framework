b1aed51eed0a26758e7c21af1de12847cabe63e9
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-12-03 20:57:17 -0600

    trigger event callbacks

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,16 @@ import { User } from "./models/User";
 const user = new User({ name: 'myname', age: 20 });
 
 user.on('change', () => {
-
+    console.log("Change #1");
 });
 
-console.log(user);
+user.on('change', () => {
+    console.log("Change #2");
+});
+
+user.on('save', () => {
+    console.log("Save triggered");
+});
+
+user.trigger('change');
+user.trigger('save');
