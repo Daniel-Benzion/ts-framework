3012377fa69e0c01672fc2a515a9fd468fca73be
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-12-16 21:59:48 -0600

    advanced generic constraint

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,3 +1,5 @@
+import { UserProps } from "./User";
+
 export class Attributes<T> {
 
     constructor(private data: T) {}
@@ -10,3 +12,13 @@ export class Attributes<T> {
         Object.assign(this.data, update);
     }
 }
+
+const attrs = new Attributes<UserProps>({
+    id: 5,
+    age: 20,
+    name: 'asdf'
+});
+
+const name = attrs.get('name');
+const age = attrs.get('age');
+const id = attrs.get('id');
