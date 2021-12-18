eed74f75beb1d881720534235a2ea11a9c229dd8
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-12-18 11:20:00 -0600

    fix context issues

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,8 @@ import { User } from "./models/User";
 
 const user = new User({name: 'new record', age: 0});
 
+console.log(user.get('name'));
+
 user.on('change', () => {
     console.log('User changed.');
 });
