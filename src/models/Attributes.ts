34d849f7654afd67f829459a097a8eda7d897c0c
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-12-20 13:48:07 -0600

    save data

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -11,14 +11,8 @@ export class Attributes<T> {
     set(update: T): void {
         Object.assign(this.data, update);
     }
+
+    getAll(): T {
+        return this.data;
+    }
 }
-
-const attrs = new Attributes<UserProps>({
-    id: 5,
-    age: 20,
-    name: 'asdf'
-});
-
-const name = attrs.get('name');
-const age = attrs.get('age');
-const id = attrs.get('id');
