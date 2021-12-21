37271de69a351e0618f87152277e0032e5ada0a1
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-12-21 15:44:16 -0600

    extend User

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -16,6 +16,10 @@ interface Events {
     trigger(eventName: string): void;
 }
 
-export class Model {
-
+export class Model<T> {
+    constructor(
+        private attrirbutes: ModelAttributes<T>,
+        private events: Events,
+        private sync: Sync<T>
+    ) {}
 }
