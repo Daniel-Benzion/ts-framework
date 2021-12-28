4832f1212810ec06366f2a6cc940d55400a13a04
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-12-28 12:43:40 -0600

    implement Users collection

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -4,4 +4,12 @@ import { User } from './User';
 export class Collection {
     models: User[] = [];
     events: Eventing = new Eventing();
+
+    get on() {
+        return this.events.on;
+    }
+
+    get trigger() {
+        return this.events.trigger;
+    }
 }
