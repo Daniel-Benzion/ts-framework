a2ea72b21fa3e1d28b7a74da001a59e9cb9931f2
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-12-29 08:32:22 -0600

    parse User JSON

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,6 @@
-// import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
-// axios.post('http://localhost:3000/users', {
-//     name: 'myname',
-//     age: 20
-// });
-
-// axios.get('http://localhost:3000/users');
-import { User } from "./models/User";
-
-const user = User.buildUser({id: 1});
-
-user.on('change', () => {
-    console.log(user);
-});
-
-user.fetch();
+axios.get('http://localhost:3000/users')
+    .then((response: AxiosResponse) => {
+        console.log(response.data);
+    });
