085dff33438b154f8b6b3880b4938f8b9cae0bed
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-12-31 13:16:35 -0600

    collection class method

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,13 @@
 //     .then((response: AxiosResponse) => {
 //         console.log(response.data);
 //     });
+
+import { User } from './models/User';
+
+const collection = User.builderUserCollection();
+
+collection.on('change', () => {
+    console.log(collection);
+});
+
+collection.fetch();
