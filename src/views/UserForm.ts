0342ce97ec485d8641e36ff8a2fc88feef88a67a
Daniel Benzion <daniel.z.benzion@gmail.com>
2022-01-05 17:12:09 -0600

    add model properties

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,6 +1,8 @@
+import { User } from '../models/User';
+
 export class UserForm {
 
-    constructor(public parent: Element) {}
+    constructor(public parent: Element, public model: User) {}
 
     eventsMap(): { [key: string]: () => void } {
         return {
