09ff546c2a0ab3d08f3411a6097aca5f5d47cc75
Daniel Benzion <daniel.z.benzion@gmail.com>
2022-01-12 21:02:35 -0600

    save data from view

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -6,13 +6,18 @@ export class UserForm extends View<User, UserProps> {
     eventsMap(): { [key: string]: () => void } {
         return {
             'click:.set-age': this.onSetAgeClick,
-            'click:.set-name': this.onSetNameClick
+            'click:.set-name': this.onSetNameClick,
+            'click:.save-model': this.onSaveClick
         };
     }
 
+    onSaveClick = (): void => {
+        this.model.save();
+    };
+
     onSetAgeClick = (): void => {
         this.model.setRandomAge();
-    }
+    };
 
     onSetNameClick = (): void => {
         const input = this.parent.querySelector('input');
@@ -22,7 +27,7 @@ export class UserForm extends View<User, UserProps> {
             this.model.set({ name });
         }
 
-    }
+    };
 
     template(): string {
         return `
@@ -30,6 +35,7 @@ export class UserForm extends View<User, UserProps> {
                 <input placeholder="${this.model.get('name')}" />
                 <button class="set-name">Change Name</button>
                 <button class="set-age">Set Random Age</button>
+                <button class="save-model">Save User</button>
             </div>
         `;
     }
