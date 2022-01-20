1112cc28a30aa048aba19fd3b87aabccdd270f30
Daniel Benzion <daniel.z.benzion@gmail.com>
2022-01-19 20:16:57 -0600

    nest View

--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -1,5 +1,7 @@
 import { User, UserProps } from "../models/User";
 import { View } from "./View";
+import { UserForm } from "./UserForm";
+import { UserShow } from "./UserShow";
 
 export class UserEdit extends View<User, UserProps> {
 
@@ -11,7 +13,8 @@ export class UserEdit extends View<User, UserProps> {
     }
 
     onRender(): void {
-        
+        new UserShow(this.regions.userShow, this.model).render();
+        new UserForm(this.regions.userShow, this.model).render();
     }
 
     template(): string {
