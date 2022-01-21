f3a0af8eeb842accbc199e62dc051a58c786f0c7
Daniel Benzion <daniel.z.benzion@gmail.com>
2022-01-20 20:41:54 -0600

    collection view

---
+++ src/views/CollectionView.ts
@@ -0,0 +1,8 @@
+import { Collection } from "../models/Collection";
+
+export abstract class CollectionView<T, K> {
+
+    constructor(public parent: Element, public collection: Collection<T, K>) {
+
+    }
+}
